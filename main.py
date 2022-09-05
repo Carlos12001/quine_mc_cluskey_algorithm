475a32c229db4174ef1366c3b6434a25100d9ac6
Carlos12001 <62664256+Carlos12001@users.noreply.github.com>
2022-09-05 12:52:52 -0600

    Step 4 finished

--- main.py
+++ main.py
@@ -33,29 +33,49 @@ def se_diferencia_un_digito(num_bin_1, num_bin_2):
     return contador == 1
 
 
+# Funcion que se encarga de remplzar el bit complementario
+def remplaza_complementos(num_bin_1, num_bin_2):
+    resultado = ""
+
+    for i in range(len(num_bin_1)):
+        if num_bin_1[i] != num_bin_2[i]:
+            resultado = resultado + "-"
+        else:
+            resultado = resultado + num_bin_1[i]
+
+    return resultado
+
+
 def prueba():
     global num_var
     num_var = 4
     lista = [12, 1, 15, 7, 6]
 
-    #Paso 1
+    # Paso 1
     print("Prueba paso 1")
     print(lista)
     lista = lista_entero_a_bin(lista)
-    print(lista,"\n\n")
+    print(lista, "\n\n")
 
     # Paso 2
     print("Prueba paso 2")
     lista = ordena_lista_por_1s(lista)
     print(lista)
-    print(lista_bin_a_entera(lista),"\n\n")
+    print(lista_bin_a_entera(lista), "\n\n")
 
     # Paso 3
     print("Prueba paso 3")
-    print("Se diferencia por un solo digito", "1000","1010")
-    print(se_diferencia_un_digito("1000","1010"))
+    print("Se diferencia por un solo digito", "1000", "1010")
+    print(se_diferencia_un_digito("1000", "1010"))
     print("Se diferencia por un solo digito", "0000", "1010")
     print(se_diferencia_un_digito("0000", "1010"), "\n\n")
+
+    # Paso 4
+    print("Prueba paso 4")
+    print("Remplaza los bits complementarios de", "1000", "1010")
+    print(remplaza_complementos("1000", "1010"))
+    print("Remplaza los bits complementarios de", "1010", "1010")
+    print(remplaza_complementos("1010", "1010"), "\n\n")
 
 
 if __name__ == "__main__":
